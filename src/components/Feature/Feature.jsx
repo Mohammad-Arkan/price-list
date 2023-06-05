6c10668f6e51dd99778e0250e2d4632e6cce6c8a
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-05 17:26:11 +0600

    update price

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Feature = ({featur}) => {
+    
+    return (
+        <div>
+           <span>{featur}</span>
+        </div>
+    );
+};
+
+export default Feature;
--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import Feature from '../Feature/Feature';
 
 const Price = (props) => {
 const { price, name, features} = props.price
     return (
         <div className='bg-indigo-400 mt-4 rounded-md p-3'>
             <h1 className='text-4xl font-bold'>Price: {price}
-            <span className='text-2xl font-semibold'> /month</span></h1>
+            <span className='text-2xl font-semibold text-white'> /month</span></h1>
             <p className='text-2xl font-medium'>Name: {name}</p>
-            <p className='text-1xl font-semibold'>Up comming features: </p>
-            <p>
+            <p className='text-1xl font-semibold text-white'>Up comming features: </p>
+            <p className=''>
+            <ch className="h-6 w-6 text-blue-500" />
                 {
-                    features.map(featur => <li>{featur}</li>)
+                    features.map((featur, idx )=> <Feature
+                    key={idx}
+                    featur={featur}
+                    ></Feature>)
 
                 }
             </p>
